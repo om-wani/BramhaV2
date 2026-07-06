2fa08092e19bdde261bcbd8724187d6db8565c10
omwani <omwani03@gmail.com>
2026-07-06 13:50:19 +0530

    feat(api): wire DbModule, UsersModule, OrgsModule, ProjectsModule into AppModule

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,6 +3,10 @@ import { ConfigModule } from '@nestjs/config'
 import { LoggerModule } from 'nestjs-pino'
 import { HealthModule } from './modules/health/health.module'
 import { AuthModule } from './modules/auth/auth.module'
+import { DbModule } from './modules/common/db/db.module'
+import { UsersModule } from './modules/users/users.module'
+import { OrgsModule } from './modules/orgs/orgs.module'
+import { ProjectsModule } from './modules/projects/projects.module'
 
 @Module({
   imports: [
@@ -31,6 +35,10 @@ import { AuthModule } from './modules/auth/auth.module'
     }),
     HealthModule,
     AuthModule,
+    DbModule,
+    UsersModule,
+    OrgsModule,
+    ProjectsModule,
   ],
 })
 export class AppModule {}
